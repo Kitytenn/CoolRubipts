b9fa3903d27de8c2a2fe551c95355647f2d708f4
Kitytenn <kitytenn@163.com>
2015-04-10 12:46:48 +0800

    substitute.rb: operate on all the files under the provided directory

--- scripts/substitute.rb
+++ scripts/substitute.rb
@@ -1,47 +1,61 @@
 #!/usr/bin/ruby
 
 unless ARGV[0] and ARGV[1] and ARGV[2]
-    puts "Usage: ruby substitute.rb <file_path> <old_content> <new_content>"
-    puts "Example: ruby substitute.rb myfile.txt path/to/file.txt hello hey"
+    puts "Usage: ruby substitute.rb <path> <old_content> <new_content>"
+    puts "Example: ruby substitute.rb mydir/ hello hey"
     exit
 end
 unless File.exist? ARGV[0]
     puts "#{ARGV[0]} does not  exist."
-    puts "Please provide the path to the file you want to manipulate."
-    exit
-end
-if File.directory? ARGV[0]
-    puts "#{ARGV[0]} is not a file but a directory.\nPlease provide the path to the file you want to manipulate."
+    puts "Please provide the path to the file or directory you want to manipulate."
     exit
 end
 
-file_path=ARGV[0]
-old_content=ARGV[1]
-new_content=ARGV[2]
+path=ARGV[0]
+@old_content=ARGV[1]
+@new_content=ARGV[2]
 
-file=File.new file_path,"r"
-if !file
-    puts "Failed to read file "+file_path
-else
-    content=""
-    file.each do |line|
-        content<<line
-    end
-    pattern=content.match /#{old_content}/
-    if !pattern
-        puts "No match found."
+def file_substitute file_path
+	file=File.new file_path,"r"
+	if !file
+	    puts "Failed to read file "+file_path
+	else
+	    content=""
+	    file.each do |line|
+	        content<<line
+	    end
+	    pattern=content.match /#{@old_content}/
+	    if !pattern
+	        puts "No match found."
+		file.close
+		exit
+	    else
+	        content=content.gsub(/#{@old_content}/,@new_content)
+	    end
+	end
 	file.close
-	exit
-    else
-        content=content.gsub(/#{old_content}/,new_content)
-    end
+	
+	file=File.new file_path,"w"
+	if !file
+	    puts "Failed to write file "+file_path
+	else
+	    file.write content
+	end
+	file.close
 end
-file.close
 
-file=File.new file_path,"w"
-if !file
-    puts "Failed to write file "+file_path
-else
-    file.write content
+def dir_substitute path
+	if path!="." and path!=".."
+		if File.directory?(path)
+			Dir.entries(path).each do |sub|
+				if sub!="." and sub!=".."
+					dir_substitute "#{path}/#{sub}"
+				end
+			end
+		else
+			file_substitute path
+		end
+	end
 end
-file.close
+
+dir_substitute path
